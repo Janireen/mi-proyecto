0f0e0eb3a5541e0386211547fc0411e6b2d5d2d2
Janireen <138075127+Janireen@users.noreply.github.com>
2024-07-05 11:55:57 -0600

    realice cambios en el componente de datos casas

---
+++ datosCasas.jsx
@@ -0,0 +1,41 @@
+const casas = [
+    {
+        nombre: 'Gryffidor',
+        valores:'Coraje, valentia y audacia',
+        animal: 'León',
+        color: 'Rojo y dorado',
+        director:'Albus Dumbledore',
+        ubicacion:'Torre Gryffidor, Castillo de Hogwarts',
+        imagen:'gryffidor.jpg',
+    },
+    {
+        nombre: 'Hufflepuff',
+        valores:'Lealtad, paciencia y amistad',
+        animal: 'Tejón',
+        color: 'Amarillo y negro',
+        director:'Pomona Sprout',
+        ubicacion:'Sótano, Castillo de Hogwarts', 
+        imagen:'hufflepuff.jpg', 
+    },
+    {
+        nombre: 'Ravenclaw',
+        valores:'Sabiduria, creativida e inteligencia',
+        animal: 'Aguila',
+        color: 'Azul y bronce',
+        director:'Filius Flitwick',
+        ubicacion:'Torre Ravenclaw, Castillo de Hogwarts', 
+        imagen:'ravenclaw.jpg', 
+    },
+    {
+        nombre: 'Slytherin',
+        valores:'Ambición, astucia y determinación',
+        animal: 'Serpiente',
+        color: 'Verde y plateado',
+        director:'Severus Snape',
+        ubicacion:'Mazmorras, Castillo de Hogwarts',
+        imagen:'slytherin.jpg',
+    },
+
+];
+
+export default casas;
